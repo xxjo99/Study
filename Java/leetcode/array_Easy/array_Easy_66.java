5e65df0d4e8668afdaa23c227f770b4463e40885
xxjo99 <xxjo0530@gmail.com>
2022-05-07 18:58:33 +0900

    220506

---
+++ Java/leetcode/array_Easy/array_Easy_66.java
@@ -0,0 +1,36 @@
+package array_Easy;
+
+public class array_Easy_66 {
+
+	public static int[] plusOne(int[] digits) {
+		int n = digits.length;
+
+		for (int i = n - 1; i >= 0; i--) {
+			digits[i]++;
+
+			if (digits[i] < 10) {
+				return digits;
+			}
+
+			digits[i] = 0;
+		}
+
+		int[] res = new int[n + 1];
+		res[0] = 1;
+
+		return res;
+	}
+
+	public static void main(String[] args) {
+		// 66. Plus One
+		int[] digits = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+		int[] result = plusOne(digits);
+
+		for (int i : result) {
+			System.out.println(i);
+		}
+
+	}
+
+}
