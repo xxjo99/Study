73635d64189213d66486c69a162496321d2499ae
xxjo99 <xxjo0530@gmail.com>
2022-05-08 15:21:25 +0900

    220508

--- Java/leetcode/arrays101/Arrays101_9.java
+++ Java/leetcode/arrays101/Arrays101_9.java
@@ -2,14 +2,29 @@ package arrays101;
 
 public class Arrays101_9 {
 
-    public void moveZeroes(int[] nums) {
-        
-    }
+	public static void moveZeroes(int[] nums) {
+		
+		int index = 0;
+		
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != 0) {
+				nums[index] = nums[i];
+				index++;
+			}
+		}
+		
+		while (index < nums.length) {
+			nums[index] = 0;
+			index++;
+		}
+ 
+	}
 
 	public static void main(String[] args) {
 		// Move Zeroes
-		int[] arr = { 0,1,0,3,12 };
-		
+		int[] arr = { 0, 1, 0, 3, 12 };
+		moveZeroes(arr);
+
 	}
 
 }
